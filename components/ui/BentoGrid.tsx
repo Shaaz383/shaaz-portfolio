eab51cdd1214fadb8131a0d0d85a373e0b2b3841
Shaaz <asadakhtar800@gmail.com>
2024-06-25 14:14:55 +0530

    Add Project section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -55,7 +55,7 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
   const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const rightLists = ["NodeJS", "NextJS", "MongoDB"];
 
   const [copied, setCopied] = useState(false);
 
@@ -69,7 +69,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "asadakhtar800@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
